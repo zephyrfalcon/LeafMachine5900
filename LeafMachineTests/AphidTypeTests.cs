81db4e39188cf5463e82107796e84892125a7635
Hans Nowak <zephyrfalcon@gmail.com>
2020-10-11 16:57:34 -0400

    more test skeletons...

---
+++ LeafMachineTests/AphidTypeTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace LeafMachineTests
+{
+    public class AphidTypeTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
