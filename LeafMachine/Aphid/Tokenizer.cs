77e2efcd7aef7608ac6ee2b192942931903c6db9
Hans Nowak <zephyrfalcon@gmail.com>
2020-10-11 19:00:43 -0400

    added simple tokenizer

---
+++ LeafMachine/Aphid/Tokenizer.cs
@@ -0,0 +1,13 @@
+﻿namespace LeafMachine.Aphid
+{
+    public class Tokenizer
+    {
+        // very basic version for now; simply splits the string on whitespace; does not take
+        // into account spaces inside string literals, comments, etc.
+        public static string[] Tokenize(string data)
+        {
+            char[] delimiters = { ' ', '\n', '\t', '\r' };
+            return data.Split(delimiters, System.StringSplitOptions.RemoveEmptyEntries);
+        }
+    }
+}
---
+++ LeafMachineTests/AphidTokenizerTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using LeafMachine.Aphid;
+
+namespace LeafMachineTests
+{
+    public class AphidTokenizerTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestTokenizer()
+        {
+            string s1 = "  1 2 3   dup drop ";
+            string[] values = { "1", "2", "3", "dup", "drop" };
+            string[] tokens = Tokenizer.Tokenize(s1);
+            Assert.AreEqual(tokens, values);
+        }
+    }
+}
