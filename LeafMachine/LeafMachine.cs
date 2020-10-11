c3f68166a87cf91d9802860d9054a40e82401bb2
Hans Nowak <zephyrfalcon@gmail.com>
2020-10-11 16:37:16 -0400

    Add project files.

---
+++ LeafMachine/LeafMachine.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LeafMachine
+{
+    public static class LeafMachine
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Machine())
+                game.Run();
+        }
+    }
+}
