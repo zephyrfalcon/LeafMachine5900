79df94a948220dd1e9a0b80bc2115010c085b658
Hans Nowak <zephyrfalcon@gmail.com>
2020-10-11 19:46:47 -0400

    first stab at parser

---
+++ LeafMachine/Aphid/Parser.cs
@@ -0,0 +1,35 @@
+﻿using LeafMachine.Aphid.Types;
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace LeafMachine.Aphid
+{
+    public class Parser
+    {
+        static string re_integer = "\\d+";
+        public static List<AphidType> Parse(string[] tokens)
+        {
+            List<AphidType> values = new List<AphidType>();
+            foreach (string token in tokens)
+            {
+                Match match = Regex.Match(token, re_integer);
+                if (match.Success)
+                {
+                    values.Add(new AphidInteger(Int32.Parse(token)));
+                    continue;
+                }
+
+                // if nothing else matches, it's a symbol
+                throw new Exception("to be implemented: symbol");
+            }
+            return values;
+        }
+
+        public static List<AphidType> TokenizeAndParse(string data)
+        {
+            string[] tokens = Tokenizer.Tokenize(data);
+            return Parse(tokens);
+        }
+    }
+}
---
+++ LeafMachineTests/AphidParserTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;  // avoid collision with NUnit.Framework.List
+using NUnit.Framework;
+using LeafMachine.Aphid;
+using LeafMachine.Aphid.Types;
+using System.Linq;
+
+namespace LeafMachineTests
+{
+    public class AphidParserTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestParser()
+        {
+            List<AphidType> values = Parser.TokenizeAndParse("1 2 3");
+            Assert.AreEqual(values.Count, 3);
+            Assert.AreEqual(values[0].ToString(), "1");
+            Assert.AreEqual(values[1].ToString(), "2");
+            Assert.AreEqual(values[2].ToString(), "3");
+
+            // we don't have map() but this appears to be the closest thing...
+            string[] actual = values.Select(x => x.ToString()).ToArray();
+            string[] expected = { "1", "2", "3" };
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
