af62ab9080f9867d7aa2f183cea79f0ab788cada
Hans Nowak <zephyrfalcon@gmail.com>
2020-10-21 22:01:07 -0400

    start setting up machine state

---
+++ LeafMachine/GraphicChar.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+
+namespace LeafMachine
+{
+    abstract public class GraphicChar
+    {
+
+    }
+
+    /* It's not clear yet what these different classes actually *mean*.
+     * I mean, we could have a HiresChar, a class for multicolor graphics, chars that actually
+     * holds multiple chars and display something different depending on the time passed or
+     * some other state, etc. Question is, do they need to *do* anything more than hold
+     * these images? The actual plotting will probably be handled differently for each, so 
+     * it might make no sense to have a Plot() method or something. (E.g. for a hires char,
+     * this would require one foreground color... which should *also* be stored in MachineState
+     * by the way... but for multicolor, we should just plot it with no extra params required.
+     * A "spinner" OTOH would require some kind of integer to determine which image we need.
+     * Needs more thought! */
+
+    public class HiresChar : GraphicChar
+    {
+        private Texture2D image;
+    }
+}
--- LeafMachine/MachineState.cs
+++ LeafMachine/MachineState.cs
@@ -1,9 +1,33 @@
-﻿namespace LeafMachine
+﻿using Microsoft.Xna.Framework;
+using LeafMachine;
+using System.Runtime.InteropServices;
+
+namespace LeafMachine
 {
+
     public class MachineState
     {
-        // TODO: 40x25 grid for characters
-        // TODO: 40x25 grid for foreground color (if hires mode)
-        // TODO: background color
+        const int WIDTH = 40;
+        const int HEIGHT = 25;
+        const int NUM_COLORS = 16;
+
+        public Color[] palette = new Color[NUM_COLORS+1];
+        // 0 is bogus but will be referred to elsewhere, meaning "background"
+        // so 1..16 are the actual colors. for now, anyway.
+        public Color bgColor;
+
+        // a 40x25 grid of characters, much like the C64
+        public GraphicChar[,] chars = new GraphicChar[WIDTH,HEIGHT];
+        public int[,] fgcolors = new int[WIDTH, HEIGHT];  // foreground colors => palette keys
+
+        public MachineState(GraphicsDeviceManager graphics)
+        {
+            // clear the 40x25 grid
+            for (int x = 0; x < 40; x++)
+                for (int y = 0; y < HEIGHT; y++)
+                    chars[x, y] = null;  // maybe EmptyChar or something?
+
+            // TODO: set the background color
+        }
     }
 }
