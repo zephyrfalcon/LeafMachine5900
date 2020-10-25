19b15a368699ff31edfa161b76a98661a769b930
Hans Nowak <zephyrfalcon@gmail.com>
2020-10-25 14:38:02 -0400

    add C64 palette, set some default colors

--- LeafMachine/MachineState.cs
+++ LeafMachine/MachineState.cs
@@ -21,14 +21,45 @@ namespace LeafMachine
         public int[,] fgcolors = new int[WIDTH, HEIGHT];  // foreground colors => palette keys
 
         // Q: Do we really need to pass a GraphicsDeviceManager? what for?
+        // we do use GraphicChar objects here, which hold actual images... or do we?
+        // shouldn't we put something else as the character type for indexes [x,y]? a char or a string...
         public MachineState(GraphicsDeviceManager graphics)
         {
+            SetC64Palette();  // for now, as a not-too-unreasonable default
+
             // clear the 40x25 grid
             for (int x = 0; x < 40; x++)
                 for (int y = 0; y < HEIGHT; y++)
                     chars[x, y] = null;  // maybe EmptyChar or something?
 
-            // TODO: set the background color
+            // set the foreground colors
+            for (int x = 0; x < WIDTH; x++)
+                for (int y = 0; y < HEIGHT; y++)
+                    fgcolors[x, y] = 15;  // light blue, in C64 palette
+
+            // set the background color
+            bgColor = 7;  // blue, in C64 palette
+        }
+
+        protected void SetC64Palette()
+        {
+            // NOTE: these indexes are one up from the ones actually used by the C64
+            palette[1] = new Color(0, 0, 0);  // black
+            palette[2] = new Color(255, 255, 255);  // white
+            palette[3] = new Color(136, 0, 0);  // red
+            palette[4] = new Color(170, 255, 238);  // cyan
+            palette[5] = new Color(204, 68, 204);  // purple
+            palette[6] = new Color(0, 204, 85);  // green
+            palette[7] = new Color(0, 0, 170);  // blue
+            palette[8] = new Color(238, 238, 119);  // yellow
+            palette[9] = new Color(221, 136, 85);  // orange
+            palette[10] = new Color(102, 68, 0);  // brown
+            palette[11] = new Color(255, 119, 119);  // pink
+            palette[12] = new Color(51, 51, 51);  // dark gray
+            palette[13] = new Color(119, 119, 119);  // med gray
+            palette[14] = new Color(170, 255, 102);  // light green
+            palette[15] = new Color(0, 136, 255);  // light blue
+            palette[16] = new Color(187, 187, 187);  // light gray
         }
     }
 }
