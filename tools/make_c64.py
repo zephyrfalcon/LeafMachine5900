e9247bc973a7fde27314b3abb75268149ac613f5
Hans Nowak <zephyrfalcon@gmail.com>
2020-10-24 20:32:23 -0400

    updated C64 charset extractor

--- tools/make_c64.py
+++ tools/make_c64.py
@@ -1,12 +1,26 @@
 #!/usr/bin/env python
 
+""" NOTE:
+c64.bin contains both C64 character sets. Characters 0..255 for the uppercase
+set, 256..511 for the lowercase one, plus graphic chars. The second part of
+each of these chunks contains the reverse characters, e.g. 1+128 contains the
+reverse of 1 (A), etc. We can omit these, since they can be trivially generated
+if necessary. So we extract positions 0..127 and 256..383, where the second
+block will end up in positions 128..255 in the generated C# code.
+
+In other words, positions 1,2,.. hold A,B,.. (uppercase)
+while positions 128,129,.. hold a,b,.. (lowercase)
+Some graphic chars will have double entries, but that is irrelevant really.
+"""
+
 data = open("c64.bin", "rb").read()
 
 bytesets = {}
 
 for i in range(256):
+    idx = i if i < 128 else i+128
     byteset = []
-    pos = i*8
+    pos = idx*8
     eight = data[pos:pos+8]
     for byte in eight:
         s = "{0:08b}".format(byte)
