813e87d5447c4cbb3eca2fc1c225214436de09b8
Hans Nowak <zephyrfalcon@gmail.com>
2020-10-28 21:16:14 -0400

    added Tools

---
+++ LeafMachine/Tools.cs
@@ -0,0 +1,12 @@
+﻿using LeafMachine;
+using System.IO;
+
+namespace LeafMachine
+{
+    public class Tools {
+        public string ExecutablePath()
+        {
+            return System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+        }
+    }
+}
