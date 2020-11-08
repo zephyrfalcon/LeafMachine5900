da69ca4e7379d67ba469755882b03584727067f9
Hans Nowak <zephyrfalcon@gmail.com>
2020-11-08 16:55:25 -0500

    first attempt at Except.cs

---
+++ LeafMachine/Expect.cs
@@ -0,0 +1,32 @@
+﻿using LeafMachine.Aphid.Types;
+using System.Collections.Generic;
+
+namespace LeafMachine
+{
+    public class Expect
+    {
+        public int ExpectInteger(string context, AphidType x)
+        {
+            if (x is AphidInteger) {
+                return (x as AphidInteger).AsInteger();
+            }
+            else throw new System.Exception($"{context}: integer expected, got {x.ToString()} instead");
+        }
+
+        public string ExpectString(string context, AphidType x)
+        {
+            if (x is AphidString) {
+                return (x as AphidString).AsString();
+            }
+            else throw new System.Exception($"{context}: string expected, got {x.ToString()} instead");
+        }
+
+        public List<AphidType> ExpectAphidList(string context, AphidType x)
+        {
+            if (x is AphidList)
+                return (x as AphidList).AsList();
+            else throw new System.Exception($"{context}: list expected, got {x.ToString()} instead");
+        }
+
+    }
+}
