b0ec8401693706833a9ac54106515f3fa1a1811e
Hans Nowak <zephyrfalcon@gmail.com>
2020-12-02 20:58:33 -0500

    Added GraphicCharSet

---
+++ LeafMachine/GraphicCharSet.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using LeafMachine.CharSets;
+using Microsoft.Xna.Framework;
+
+namespace LeafMachine
+{
+    /* A GraphicCharSet holds a CharSet instance and will keep a dictionary of actual *images* (in Texture2D
+       form), keyed by character/name.
+    */
+    public class GraphicCharSet
+    {
+        GraphicsDeviceManager _graphics;
+        CharSet charset;
+        Dictionary<string, GraphicChar> graphicChars;
+
+        public GraphicCharSet(GraphicsDeviceManager graphics, CharSet aCharSet)
+        {
+            _graphics = graphics;
+            charset = aCharSet;
+            Init();
+        }
+
+        public GraphicChar Get(string name)
+        {
+            return graphicChars[name];
+        }
+
+        private void Init()
+        {
+            graphicChars = new Dictionary<string, GraphicChar> { };
+            foreach (string name in charset.KnownChars()) {
+                graphicChars[name] = new HiresChar(_graphics, charset.BitmapForChar(name));
+            }
+        }
+    }
+}
