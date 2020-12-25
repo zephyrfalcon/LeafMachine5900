8e61a14870994e20ef68e0c842d75f5f75a696bd
Hans Nowak <zephyrfalcon@gmail.com>
2020-12-25 15:44:52 -0500

    added CharBitmap, HiresCharBitmap, MultiColorCharBitmap

---
+++ LeafMachine/CharBitmap.cs
@@ -0,0 +1,38 @@
+﻿namespace LeafMachine
+{
+    /* A CharBitmap is really just a wrapper around int[], except we have different versions
+       for hires (64 bits) and multicolor (32 color values). 
+       Using arrays of CharBitmap elsewhere allows us to mix hires and multicolor bitmaps
+       unambiguously.
+    */
+
+    public abstract class CharBitmap
+    {
+        public virtual int[] GetValues()
+        {
+            throw new System.Exception("not implemented");
+        }
+    }
+
+    public class HiresCharBitmap : CharBitmap
+    {
+        int[] bitmap64;
+        public HiresCharBitmap(int[] bits)
+        {
+            if (bits.Length != 64) throw new System.Exception($"number of bits must be exactly 64, got {bits.Length}");
+            bitmap64 = bits;
+        }
+        public override int[] GetValues() { return bitmap64; }
+    }
+
+    public class MultiColorCharBitmap : CharBitmap
+    {
+        int[] bitmap32;
+        public MultiColorCharBitmap(int[] values)
+        {
+            if (values.Length != 32) throw new System.Exception($"number of values must be exactly 32, got {values.Length}");
+            bitmap32 = values;
+        }
+        public override int[] GetValues() { return bitmap32; }
+    }
+}
