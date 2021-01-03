b0e5f1b474de003e76a772ad65d1a6751c851d40
Hans Nowak <zephyrfalcon@gmail.com>
2021-01-03 00:12:07 -0500

    added ShortcutExpander

---
+++ LeafMachine/Aphid/Shortcuts.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+using System;
+using System.Collections.Generic;
+
+
+namespace LeafMachine.Aphid
+{
+    public class ShortcutExpander
+    {
+        static string re_get_variable = @"^\$([^!]\S*)$";
+        static string re_set_variable = @"^\$!(\S+)$";
+
+        // If the token is a shortcut, return a list of tokens (strings) that it expands to.
+        // Otherwise, return null.
+        public List<string> Expand(string token)
+        {
+            Match match;
+            match = Regex.Match(token, re_set_variable);
+            if (match.Success) {
+                // $!foo => :foo setvar
+                return new List<string> { $":{match.Groups[1]}", "setvar" };
+            }
+            match = Regex.Match(token, re_get_variable);
+            if (match.Success) {
+                // $foo => :foo getvar
+                return new List<string> { $":{match.Groups[1]}", "getvar" };
+            }
+            return null;
+        }
+    }
+}
---
+++ LeafMachineTests/AphidShortcutsTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using LeafMachine.Aphid;
+
+namespace AphidShortcutsTests
+{
+    public class Tests
+    {
+        ShortcutExpander exp;
+
+        [SetUp]
+        public void Setup()
+        {
+            exp = new ShortcutExpander();
+        }
+
+        [Test]
+        public void TestExpandVariables()
+        {
+            Assert.AreEqual(null, exp.Expand("bogus"));
+            Assert.AreEqual(new List<string> { ":foo", "getvar" }, exp.Expand("$foo"));
+            Assert.AreEqual(new List<string> { ":bar", "setvar" }, exp.Expand("$!bar"));
+        }
+    }
+}
