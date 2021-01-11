55288a80f9f1b6fdc89f386fb45f50809cee2a17
Hans Nowak <zephyrfalcon@gmail.com>
2021-01-10 22:31:11 -0500

    skeleton for Spinner class

---
+++ LeafMachine/Spinner.cs
@@ -0,0 +1,25 @@
+﻿
+namespace LeafMachine
+{
+
+    public class Spinner
+    {
+        int _tixToChange;
+        int _currentTix = 0;
+        CharInfo[] _chars;
+        int _x, _y;
+
+        public Spinner(int x, int y, CharInfo[] chars, int tixToChange ) {
+            _tixToChange = tixToChange;
+            _currentTix = 0;
+            _chars = chars;
+            _x = x;
+            _y = y;
+        }
+
+        public CharInfo GetCharInfo(int x, int y, int tix)
+        {
+            throw new System.Exception("not implemented");
+        }
+    }
+}
